f651582e0ee2562b1b467a891a495cb929b76dff
김지훈 <kigpand@gmail.com>
2025-12-03 23:28:22 +0900

    test: layout, stack test

---
+++ app/(tabs)/routine/index.tsx
@@ -0,0 +1,36 @@
+import { Stack } from "expo-router";
+import { Text, TouchableOpacity, View } from "react-native";
+
+const categories = [
+  { id: "chest", name: "가슴" },
+  { id: "back", name: "등" },
+  { id: "shoulder", name: "어깨" },
+  { id: "legs", name: "하체" },
+  { id: "arms", name: "팔" },
+];
+
+export default function Routine() {
+  return (
+    <View style={{ flex: 1, padding: 20 }}>
+      <Stack.Screen options={{ title: "운동 시작" }} />
+
+      <Text style={{ fontSize: 24, fontWeight: "600", marginBottom: 20 }}>
+        운동 카테고리
+      </Text>
+
+      {categories.map((c) => (
+        <TouchableOpacity
+          key={c.id}
+          style={{
+            padding: 16,
+            backgroundColor: "#eee",
+            borderRadius: 10,
+            marginBottom: 12,
+          }}
+        >
+          <Text style={{ fontSize: 18 }}>{c.name}</Text>
+        </TouchableOpacity>
+      ))}
+    </View>
+  );
+}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,8 +5,9 @@ import {
 } from "@react-navigation/native";
 import "react-native-reanimated";
 
-import WorkoutListScreen from "@/components/WorkListScreen";
 import { useColorScheme } from "@/hooks/use-color-scheme";
+import { Stack } from "expo-router";
+import { Button, StyleSheet, View } from "react-native";
 
 export const unstable_settings = {
   anchor: "(tabs)",
@@ -14,11 +15,32 @@ export const unstable_settings = {
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
-  console.log("tes");
 
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <WorkoutListScreen />
+      <View
+        style={styles.layout}
+        accessible={true}
+        accessibilityLabel="앱 관련 기능 메뉴"
+        accessibilityHint="운동을 시작하거나 최근 기록을 확인할 수 있습니다"
+      >
+        <Button title="운동 시작" accessibilityLabel="운동을 시작하기" />
+        <Button title="운동 기록" accessibilityLabel="나의 운동 기록 보기" />
+        <Button title="최근 기록" accessibilityLabel="최근 운동 기록 보기" />
+      </View>
+      <Stack
+        screenOptions={{
+          headerShown: false,
+        }}
+      />
     </ThemeProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  layout: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center",
+  },
+});
